d21da00570df481a205a6bceb3d90a42f791f6d3
Mistire <mdwithgod@gmail.com>
2024-05-22 10:37:04 +0300

    first commit

---
+++ netflix-clone/src/Components/Header/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./header.css";
+import NetflixLogo from "../../assets/images/netflix.png";
+
+const Header = () => {
+  return (
+    <div className={`header_outer_container `}>
+      {/* ${show && "nav__black"} */}
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix-Logo" width="100" />
+            </li>
+
+            <li>Netflix</li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        {/* <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
